37f6c7863bc263de66473c00aae616c360dcdd4e
gustavo <gustavodasilvama10@gmail.com>
2023-08-02 19:39:34 -0300

    reform

--- index.js
+++ index.js
@@ -1,40 +1,13 @@
-require("dotenv").config()
-const bodyPaser= require("body-parser")
 const express = require("express")
 const app = express()
-const matter = require("./matter/Matter")
-const activities = require("./activities/Activities")
+require("dotenv").config()
+const ejs = require("ejs")
 
-const activitiesControl = require("./activities/ActivitiesControl")
-const matterControl = require("./matter/MatterControl")
-
-async function database(){
-  
-  const connection = await require("./databases/database")
-  try{
-    await connection.authenticate()
-  
-    await console.log("Banco De Dados Conectado")
-  }catch(err){
-    console.log(err)
-  }
-}
-database()
-
-
-app.use(bodyPaser.urlencoded({extended: false}))
-app.use(bodyPaser.json())
-
-app.set("view engime", "ejs")
-
-app.use("/", activitiesControl)
-app.use("/", matterControl)
-
+app.set("view engine", "ejs")
 app.get("/", (req, res)=> {
-  res.render("index.ejs")
+    res.render("home.ejs")
 })
-
 const PORT = process.env.PORT || 8081
 app.listen(PORT, ()=> {
-  console.log("Servidor rodando na porta: ", PORT)
+    console.log("Rodando!! na porta: "+ PORT)
 })
